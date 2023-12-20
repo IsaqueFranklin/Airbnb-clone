2e4c68d8d39902f4bd163a7e0399c98f67a9a054
Isaque <isaquefrankli@gmail.com>
2023-12-19 22:33:03 -0300

    next bit of code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,16 @@ import './App.css'
 function App() {
 
   return (
-    <div className='bg-red-500'>Test</div>
+    <div >
+      <header>
+        <a href="" className='logo'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+          </svg>
+          <span>airbnb</span>
+        </a>
+      </header>
+    </div>
   )
 }
 
