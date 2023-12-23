20430ab67461c9a8d84dc7107c84db85e413b74a
Isaque <isaquefrankli@gmail.com>
2023-12-23 16:59:11 -0300

    upload photo by link done

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
+
 const app = express();
 require('dotenv').config();
 
@@ -17,6 +19,7 @@ const jwtSecret = 'minhavelhacomproumeujantarsopauvanozespãodorminocomsaladaden
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
@@ -87,6 +90,16 @@ app.get('/profile', (req, res) => {
 app.post('/logout', (req, res) => {
     res.cookie('token', '').json(true);
 });
+
+app.post('/upload-by-link', async (req, res) => {
+    const {link} = req.body;
+    const newName = 'photo' + Date.now()+'.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname+'/uploads/'+newName,
+    });
+    res.json(await newName);
+})
 
 //Init server
 
