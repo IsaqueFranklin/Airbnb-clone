db5710d9c409508e55d062ade492d003e1368675
Isaque <isaquefrankli@gmail.com>
2023-12-28 21:29:52 -0300

    gathering places from database

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {Link, useParams} from 'react-router-dom';
 import ProfileNav from '../components/ProfileNav';
+import axios from 'axios';
 
 export default function PlacesPage() {
+
+    const [places, setPlaces] = useState([]);
+
+    useEffect(() => {
+       axios.get('/places').then(({data}) => {
+        setPlaces(data)
+       });
+    }, []);
 
     return (
         <div>
             <ProfileNav />
             <div className="text-center">
-                List of all added places.
                 <br/>
                 <Link className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full" to={'/profile/places/new'}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
@@ -16,6 +24,14 @@ export default function PlacesPage() {
                     </svg>
                     Add new place
                 </Link>
+            </div>
+            <div className="mt-4">
+                {places.length > 0 && places.map(place => (
+                    <div className='bg-gray-200 p-4 rounded-2xl mb-4'>
+
+                        {place.title}
+                    </div>
+                ))}
             </div>
         </div>
     )
