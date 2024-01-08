23c83fcf0cce3673c5c0a1a74e6537d6ad363fa7
Isaque <isaquefrankli@gmail.com>
2024-01-08 19:30:14 -0300

    componentization of place-gallery and address

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,10 +1,31 @@
-import { useParams } from "react-router-dom"
+import {useState, useEffect} from 'react';
+import { useParams } from "react-router-dom";
+import axios from 'axios';
 
 export default function BookingPage() {
 
     const {id} = useParams();
+    const [booking, setBooking] = useState(null);
+
+    useEffect(() => {
+        if(id) {
+            axios.get('/bookings').then(response => {
+                const foundBooking = response.data.find(({_id}) => _id === id)
+
+                if(foundBooking) {
+                    setBooking(foundBooking);
+                }
+            })
+        }
+    }, []);
+
+    if(!booking) {
+        return '';
+    }
 
     return (
-        <div>Single booking {id}</div>
+        <div>
+            
+        </div>
     )
 }
